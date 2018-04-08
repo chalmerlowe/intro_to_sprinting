5d06d2b799d95b28789c304510d2dd77a66cc557
jeffrey k eliasen <jeff@jke.net>
2018-04-08 10:00:26 -1000

    move toc to same directory as .md files

--- tools/build-nav.py
+++ tools/build-nav.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import collections
+import importlib.util
 import logging
 import os
 import re
@@ -10,31 +11,7 @@ log = logging.getLogger(__name__)
 
 TOC_MAX_DEPTH = int(os.environ.get('TOC_MAX_DEPTH', '3'))
 LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING')
-
-
-HIERARCHY = ('Table of Contents', './README.md', (
-    ('Course Overview and Preparation', './prereq_overview.md', (
-        ('Instructor Preparation', './prereq_instructor.md', ()),
-        ('Student Preparation', './prereq_student.md', ()),
-    )),
-    ('Environment Set-up', './environment_overview.md', (
-        ('Installing the Software You\'ll Need', './installing_tools.md', ()),
-        ('Setting up Virtual Environments', './virtual_environments.md', ()),
-        ('Setting up GitHub and Forking a Repository', './github_setup.md', ()),
-        ('Setting up Git', './git_config.md', ()),
-    )),
-    ('Using Git', './git_overview.md', (
-        ('Git Concepts', './git_concepts.md', ()),
-        ('Cloning a Repository', './git_cloning.md', ()),
-        ('Git Primary Workflow: Add, Commit, Push', './git_main_lifecycle.md', ()),
-        ('Git Common Operations', './git_common_operations.md', ()),
-        ('Branching and Merging', './git_branch_merge.md', ()),
-    )),
-    ('Using GitHub', './github_overview.md', (
-        ('GitHub Concepts', './github_concepts.md', ()),
-        ('Submitting a Pull Request', './github_submit_pull_request.md', ()),
-    )),
-))
+DOCS_DIR = os.environ.get('DOCS_DIR', 'class_materials')
 
 AUTO_SECTION_TAGS = ('<!-- begin auto-generated {0} section -->', '<!-- end auto-generated section -->')
 TITLE_OPEN_TAG = AUTO_SECTION_TAGS[0].format('title')
@@ -51,8 +28,12 @@ TITLE_REGEX = re.compile(f'\s*{TITLE_OPEN_TAG}\s*.*?\s*{AUTO_SECTION_TAGS[1]}\s*
 NAV_REGEX = re.compile(f'\s*{NAV_OPEN_TAG}\s*.*?\s*{AUTO_SECTION_TAGS[1]}\s*', re.MULTILINE | re.DOTALL)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-CONTENT_DIR = os.path.join(BASE_DIR, 'class_materials')
+CONTENT_DIR = os.path.join(BASE_DIR, DOCS_DIR)
 
+toc_spec = importlib.util.spec_from_file_location('toc', os.path.join(CONTENT_DIR, 'toc.py'))
+toc = importlib.util.module_from_spec(toc_spec)
+toc_spec.loader.exec_module(toc)
+HIERARCHY = toc.HIERARCHY
 ROOT_ELEMENT = HIERARCHY[0][1]
 
 Node = collections.namedtuple('Node', ['index', 'title', 'filename', 'parent', 'children'])
